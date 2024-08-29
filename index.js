dbf07dde98d170b816a3779a954c78e2a3d78710
nekron2323 <goncharov.uv@gmail.com>
2024-08-29 23:52:14 +0300

    options

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 require('dotenv').config()
 const TelegramApi = require('node-telegram-bot-api')
 const TOKEN = process.env.TOKEN
+const {nums_options, replay_options} = require('./options')
 
 const bot = new TelegramApi(TOKEN, { polling: true })
-
 const chats = {}
-
-const options = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{ text: '1', callback_data: 1 }, { text: '2', callback_data: 2 }, { text: '3', callback_data: 3 }],
-            [{ text: '4', callback_data: 4 }, { text: '5', callback_data: 5 }, { text: '6', callback_data: 6 }],
-            [{ text: '7', callback_data: 7 }, { text: '8', callback_data: 8 }, { text: '9', callback_data: 9 }],
-            [{ text: '0', callback_data: 0 }]
-            
-        ]
-    })
-}
-
-const replay_options = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{ text: 'Играть еще раз!', callback_data: '/game' }]            
-        ]
-    })
-}
-
 const startGame = async (chatId) => {
     chats[chatId] = Math.floor(Math.random() * 10)
-    return bot.sendMessage(chatId, 'Отгадывай', options)
+    return bot.sendMessage(chatId, 'Отгадывай', nums_options)
 }
 
 const start = () => {
@@ -56,7 +35,7 @@ const start = () => {
         const chatId = msg.message.chat.id
         if (data === '/game') return startGame(chatId)
         if (data == chats[chatId]) return bot.sendMessage(chatId, `Поздравляю! Вы угадали`, replay_options)
-        return bot.sendMessage(chatId, `Неверно! Попробуйте еще раз`, options)
+        return bot.sendMessage(chatId, `Неверно! Попробуйте еще раз`, nums_options)
     })
 }
 
